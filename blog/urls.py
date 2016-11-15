c13a5dc8041232ffc34cfcde8211b89dfd34368e
lourenzo86 <lourenzo86@outlook.com>
2016-11-15 15:33:21 -0200

    Added views to create/edit blog post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     url(r'^$', post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', post_detail, name='post_detail'),
-
+    url(r'^post/new/$', post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import *
-
+from .forms import *
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -11,3 +11,29 @@ def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',locals())
 
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)#'commit=False' faz com que não salve por enquanto
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', locals())
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html',locals())
